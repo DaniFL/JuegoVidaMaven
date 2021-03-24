e02bdb131120e984c68dff18e9b4e17d0ce657d3
Dani2002@bitbucket.org <danielfernandezlopez75@gmail.com>
2021-03-24 14:31:33 +0100

    JVMN

--- Juego-Vida/src/main/java/pr2/org/aplicacion/Principal.java
+++ Juego-Vida/src/main/java/pr2/org/aplicacion/Principal.java
@@ -5,28 +5,30 @@ import java.util.concurrent.TimeUnit;
 
 import pr2.org.dominio.Tablero;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.InterruptedException;
 
 public class Principal {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         try {
             Tablero tablero = new Tablero();
             System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
-            tablero.leerEstadoActual();
+            Tablero.leerEstadoActual();
             System.out.println(tablero);
         for(int i = 0; i <= 5; i++){
             TimeUnit.SECONDS.sleep(1);
-            tablero.transitarAlEstadoSiguiente();
+            Tablero.transitarAlEstadoSiguiente();
             System.out.println(tablero);
         }
             System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
-            tablero.generarEstadoActualPorMontecarlo();
+            Tablero.generarEstadoActualPorMontecarlo();
             System.out.println(tablero);
 
         for(int i = 0; i <= 15; i++){
         TimeUnit.SECONDS.sleep(1);
-        tablero.transitarAlEstadoSiguiente();
+        Tablero.transitarAlEstadoSiguiente();
         System.out.println(tablero);
         }
     }   catch(InterruptedException e){
--- Juego-Vida/src/main/java/pr2/org/dominio/Tablero.java
+++ Juego-Vida/src/main/java/pr2/org/dominio/Tablero.java
@@ -1,8 +1,11 @@
 package pr2.org.dominio;
 
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /*Las reglas son las habituales:
@@ -14,8 +17,8 @@ c) El resto de células pasan a estar muertas en el estado siguiente.
 
 public class Tablero {
     private static int DIMENSION = 32;
-         public int[][] estadoActual = new int[DIMENSION][DIMENSION]; //matriz que representa el estado actual.
-         public int[][] estadoSiguiente = new int[DIMENSION][DIMENSION]; // Matriz que representa el estado siguiente.
+         public static int[][] estadoActual = new int[DIMENSION][DIMENSION]; //matriz que representa el estado actual.
+         public static int[][] estadoSiguiente = new int[DIMENSION][DIMENSION]; // Matriz que representa el estado siguiente.
          private Scanner leerfichero;
          
          public void instaurarMatriz() throws FileNotFoundException{
@@ -34,38 +37,134 @@ public class Tablero {
                  System.out.println(fila);
             }
         }
-         public void leerEstadoActual(){
-         
-        
-        
+         public static void leerEstadoActual() throws IOException{
+            
+                    File fichero = new File("Matriz.txt");
+                    Scanner s = new Scanner(fichero);
+                    	
+                    int i =0;
+                    while (s.hasNextLine()) {
+                        String fila = s.nextLine(); 	
+                            
+                         for (int r=0; r<fila.length()-1; r++) {
+                            estadoActual[i][r]= Character.getNumericValue(fila.charAt(r));
+                         }
+                         i++;
+                        System.out.println(fila);
+                     }
+                    
+                    s.close();
+                    
+            
+    
+                    
         }
+        
+        
+        
          // La secuencia de ceros y unos del fichero es guardada
          // en ‘estadoActual‘ y, utilizando las reglas del juego
          // de la vida, se insertan los ceros y unos
          // correspondientes en ‘estadoSiguiente‘.
-         /********************************************************
+         /********************
           * Genera un estado inicial aleatorio. Para cada celda
           * genera un número aleatorio en el intervalo [0, 1). Si
           * el número es menor que 0,5, entonces la celda está
           * inicialmente viva. En caso contrario, está muerta.
-          *******************************************************/
-          public void generarEstadoActualPorMontecarlo(){}
+          *******************/
+          public static void generarEstadoActualPorMontecarlo(){}
           // La secuencia de ceros y unos generada es guardada
           // en ‘estadoActual‘ y, utilizando las reglas del juego
           // de la vida, se insertan los ceros y unos
           // correspondientes en ‘estadoSiguiente‘.
-         /********************************************************
+         /********************
           * Transita al estado siguiente según las reglas del
           * juego de la vida.
-          ********************************************************/
-         public void transitarAlEstadoSiguiente(){}
+         * @throws IOException
+          ********************/
+         public static void transitarAlEstadoSiguiente() throws IOException{
+            int nUnos=0;
+            
+            for (int r=1; r<(DIMENSION-2); r++) {
+                for(int i=1; i<(DIMENSION-2) ; i++) {
+                    System.out.print(estadoSiguiente[r][i]);
+                    if(estadoActual[r][i]==1) {	
+                        nUnos = 0;
+                                
+                                if(estadoActual[r+1][i-1]==1) {
+                                    nUnos++;
+                                }
+                                    if(estadoActual[r+1][i]==1) {
+                                        nUnos++;
+                                    }	
+                                        if(estadoActual[r+1][i+1]==1) {
+                                            nUnos++;
+                                        }	
+                                            if(estadoActual[r][i-1]==1) {
+                                                nUnos++;
+                                            }	
+                                                if(estadoActual[r][i+1]==1) {
+                                                    nUnos++;
+                                                }	
+                                                    if(estadoActual[r-1][i-1]==1) {
+                                                        nUnos++;
+                                                    }	
+                                                        if(estadoActual[r-1][i]==1) {
+                                                            nUnos++;
+                                                        }	
+                                                            if(estadoActual[r-1][i+1]==1) {
+                                                                nUnos++;
+                                                                
+                                                            }if(nUnos==2 || nUnos==3) estadoSiguiente[r][i]=1;
+                                                                
+                                                            else estadoSiguiente[r][i]=0;
+                                                            System.out.println();
+                    }else{
+                        nUnos = 0;
+                                if(estadoActual[r+1][i-1]==1) {
+                                    nUnos++;
+                                }
+                                    if(estadoActual[r+1][i]==1) {
+                                        nUnos++;
+                                    }	
+                                        if(estadoActual[r+1][i+1]==1) {
+                                            nUnos++;
+                                        }	
+                                            if(estadoActual[r][i-1]==1) {
+                                                nUnos++;
+                                            }	
+                                                if(estadoActual[r][i+1]==1) {
+                                                    nUnos++;
+                                                }	
+                                                    if(estadoActual[r-1][i-1]==1) {
+                                                        nUnos++;
+                                                    }	
+                                                        if(estadoActual[r-1][i]==1) {
+                                                            nUnos++;
+                                                        }	
+                                                            if(estadoActual[r-1][i+1]==1) {
+                                                                nUnos++;
+                                                                
+                                                            }if(nUnos==3) estadoSiguiente[r][i]=1;
+                                                                
+                                                            else estadoSiguiente[r][i]=0;
+    
+                    }
+                }
+            }
+
+            System.out.println();
+			
+            estadoSiguiente= estadoActual;
+         
+    }
          // La variable ‘estadoActual‘ pasa a tener el contenido
          // de ‘estadoSiguiente‘ y, éste útimo atributo pasar a
          // reflejar el siguiente estado.
-         /*******************************************************
+         /*******************
           * Devuelve, en modo texto, el estado actual.
           * @return el estado actual.
-          *******************************************************/
+          *******************/
          
 
           @Override
